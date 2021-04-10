704f3f1018e3978a53e3cf62c1ba6e632980bcc0
eguiadaniel <eguiadaniel@users.noreply.github.com>
2021-04-10 14:14:48 +0200

    commented user related lines in Client App. Still Server authentication not working

--- client/src/services/authentication.js
+++ client/src/services/authentication.js
@@ -1,24 +1,25 @@
 import api from './api';
 
 export const signIn = async data => {
-  const response = await api.post('/authentication/sign-in', data);
-  const body = response.data;
-  const user = body.user;
-  return user;
+    const response = await api.post('/authentication/sign-in', data);
+    const body = response.data;
+    const user = body.user;
+    return user;
 };
 
 export const signUp = async data => {
-  const response = await api.post('/authentication/sign-up', data);
-  const body = response.data;
-  const user = body.user;
-  return user;
+    console.log(data)
+    const response = await api.post('/authentication/sign-up', data);
+    const body = response.data;
+    const user = body.user;
+    return user;
 };
 
-export const signOut = async () => {
-  await api.post('/authentication/sign-out');
+export const signOut = async() => {
+    await api.post('/authentication/sign-out');
 };
 
-export const verify = async () => {
-  const response = await api.get('/authentication/verify');
-  return response.data.user;
+export const verify = async() => {
+    const response = await api.get('/authentication/verify');
+    return response.data.user;
 };
--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -4,63 +4,65 @@ const { Router } = require('express');
 
 const bcryptjs = require('bcryptjs');
 const User = require('./../models/user');
+const Individual = require('./../models/individual');
+const Organisation = require('./../models/organisation');
 
 const router = new Router();
 
 router.post('/sign-up', (req, res, next) => {
-  const { name, email, password, role } = req.body;
-  bcryptjs
-    .hash(password, 10)
-    .then((hash) => {
-      return User.create({
-        name,
-        email,
-        passwordHashAndSalt: hash,
-        role
-      });
-    })
-    .then((user) => {
-      req.session.userId = user._id;
-      res.json({ user });
-    })
-    .catch((error) => {
-      next(error);
-    });
+    const { name, email, password, role } = req.body;
+    bcryptjs
+        .hash(password, 10)
+        .then((hash) => {
+            return User.create({
+                name,
+                email,
+                passwordHashAndSalt: hash,
+                role
+            });
+        })
+        .then((user) => {
+            req.session.userId = user._id;
+            res.json({ user });
+        })
+        .catch((error) => {
+            next(error);
+        });
 });
 
 router.post('/sign-in', (req, res, next) => {
-  let user;
-  const { email, password } = req.body;
-  User.findOne({ email })
-    .then((document) => {
-      if (!document) {
-        return Promise.reject(new Error("There's no user with that email."));
-      } else {
-        user = document;
-        return bcryptjs.compare(password, user.passwordHashAndSalt);
-      }
-    })
-    .then((result) => {
-      if (result) {
-        req.session.userId = user._id;
-        res.json({ user });
-      } else {
-        return Promise.reject(new Error('Wrong password.'));
-      }
-    })
-    .catch((error) => {
-      next(error);
-    });
+    let user;
+    const { email, password } = req.body;
+    User.findOne({ email })
+        .then((document) => {
+            if (!document) {
+                return Promise.reject(new Error("There's no user with that email."));
+            } else {
+                user = document;
+                return bcryptjs.compare(password, user.passwordHashAndSalt);
+            }
+        })
+        .then((result) => {
+            if (result) {
+                req.session.userId = user._id;
+                res.json({ user });
+            } else {
+                return Promise.reject(new Error('Wrong password.'));
+            }
+        })
+        .catch((error) => {
+            next(error);
+        });
 });
 
 router.post('/sign-out', (req, res, next) => {
-  req.session.destroy();
-  res.json({});
+    req.session.destroy();
+    res.json({});
 });
 
 router.get('/verify', (req, res) => {
-  const user = req.user || null;
-  res.json({ user: user });
+    const user = req.user || null;
+    res.json({ user: user });
 });
 
 module.exports = router;
