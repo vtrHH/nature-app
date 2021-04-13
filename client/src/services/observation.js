5447c7a1d41d73d1340530010154c927429c694f
eguiadaniel <eguiadaniel@users.noreply.github.com>
2021-04-13 21:16:58 +0200

    new changes

--- client/src/services/observation.js
+++ client/src/services/observation.js
@@ -1,25 +1,24 @@
 import api from './api';
 
 //loads an observation based on the id
-export const loadObservation = async (id) => {
-  const response = await api.get(`/observation/${id}`);
-  return response.data.observation;
+export const loadObservation = async(id) => {
+    const response = await api.get(`/observation/${id}`);
+    return response.data.observation;
 };
 
-export const createObservation = async => {
-  const response = await api.post('/observation/');
-  return response.data.observation;
+export const createObservation = async(data) => {
+    const response = await api.post('/observation/', data);
+    return response.data.observation;
 };
 
-export const editObservation = async (id) => {
-  const response = await api.patch(`/observation/${id}/edit`);
-  return response.data.observation;
-};  
+export const editObservation = async(id) => {
+    const response = await api.patch(`/observation/${id}/edit`);
+    return response.data.observation;
+};
 
 //lists all observation 
 //(maybe filtered, sorted, etc in th serverside)
-export const listOfObservations = async ()=> {
-  const response = await api.get('/observation/list');
-  return response.data.observations;
+export const listOfObservations = async() => {
+    const response = await api.get('/observation/list');
+    return response.data.observations;
 };
-
