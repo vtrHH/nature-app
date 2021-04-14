0037bf36c5bb3917b9d335d1c5b0965e58efd264
eguiadaniel <eguiadaniel@users.noreply.github.com>
2021-04-14 14:43:49 +0200

    ObservationRoute crashes the server

--- server/models/observation.js
+++ server/models/observation.js
@@ -3,40 +3,43 @@
 const mongoose = require('mongoose');
 
 const observationSchema = new mongoose.Schema({
-  date: {
-    type: Date,
-    required: true
-  },
-  location: {
-    coordinates: [
-      {
-        type: Number,
-        min: -180,
-        max: 180
-      }
-    ],
-    type: {
-      type: String,
-      default: 'Point',
-      required: true
+    date: {
+        type: Date,
+        required: true
+    },
+    location: {
+        coordinates: [{
+            type: Number,
+            min: -180,
+            max: 180
+        }],
+        type: {
+            type: String,
+            default: 'Point',
+            required: true
+        }
+        // required: true
+    },
+    bird: {
+        type: String,
+        required: true
+    },
+    picture: {
+        type: String,
+        required: true
+    },
+    verified: {
+        type: Boolean
+    },
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     }
-    // required: true
-  },
-  bird: {
-    type: String,
-    required: true
-  },
-  picture: {
-    type: String,
-    required: true
-  },
-  verified: {
-    type: Boolean
-  },
-  creator: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }
+}, {
+    timestamps: {
+        createdAt: 'addedDate',
+        updatedAt: 'editDate'
+    }
 });
 
 module.exports = mongoose.model('Observation', observationSchema);
--- server/routes/observation.js
+++ server/routes/observation.js
@@ -1,74 +1,75 @@
 'use strict';
 
-const express = require('express');
+const { Router } = require('express');
+// const express = require('express');
 
 //const Bird = require('./../models/bird');
 //const User = require('./../models/user');
 const Observation = require('./../models/observation');
 
 const routeGuard = require('./../middleware/route-guard');
-const fileUpload = require('./../middleware/file-upload');
+// const fileUpload = require('./../middleware/file-upload');
 //const sendEmail = require('./../utilities/send-email');
 
-const router = new express.Router();
+const router = new Router();
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    const observation = await Observation.findById(req.params.id);
-    res.json({ observation });
-  } catch (error) {
-    next(error);
-  }
+router.get('/:id', async(req, res, next) => {
+    try {
+        const observation = await Observation.findById(req.params.id);
+        res.json({ observation });
+    } catch (error) {
+        next(error);
+    }
 });
 
-router.post('/', routeGuard, async (req, res, next) => {
-  try {
-    //fileUpload.single('picture'),
-    const { location, bird, creator } = req.body;
-    //add picture
-    const observation = await Observation.create({
-      location,
-      bird,
-      creator,
-      picture
-    });
-    res.json({ observation });
-  } catch (error) {
-    next(error);
-  }
+router.post('/', routeGuard, async(req, res, next) => {
+    try {
+        //fileUpload.single('picture'),
+        const { location, bird, date } = req.body;
+        const creator = req.user._id;
+        //add picture
+        const observation = await Observation.create({
+            location,
+            bird,
+            date,
+            creator
+            // picture
+        });
+        res.json({ observation });
+    } catch (error) {
+        next(error);
+    }
 });
 
 router.patch(
-  '/:id/edit',
-  routeGuard,
-  //fileUpload.single('picture'),
-  async (req, res, next) => {
-    const { location, bird, picture } = req.body;
-    try {
-      //add picture
-      const observation = await Observation.findByIdAndUpdate(
-        req.params.id,
-        {
-          $set: {
-            location,
-            bird,
-            picture
-          }
-        },
-        { new: true }
-      );
-      res.json({ observation });
-    } catch (error) {
-      next(error);
+    '/:id/edit',
+    routeGuard,
+    //fileUpload.single('picture'),
+    async(req, res, next) => {
+        const { location, bird, picture } = req.body;
+        try {
+            //add picture
+            const observation = await Observation.findByIdAndUpdate(
+                req.params.id, {
+                    $set: {
+                        location,
+                        bird,
+                        picture
+                    }
+                }, { new: true }
+            );
+            res.json({ observation });
+        } catch (error) {
+            next(error);
+        }
     }
-  }
 );
 
-router.get('/list', async (req, res, next) => {
-  try {
-    const observations = await Observation.find().limit(5);
-    res.json({ observations });
-  } catch (error) {
-    next(error);
-  }
+router.get('/list', async(req, res, next) => {
+    try {
+        const observations = await Observation.find().limit(5);
+        res.json({ observations });
+    } catch (error) {
+        next(error);
+    }
 });
