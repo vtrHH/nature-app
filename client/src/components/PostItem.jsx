1b3b6e97c25f7a46cb70b8d7e368ff4cd4fe785b
eguiadaniel <eguiadaniel@users.noreply.github.com>
2021-04-23 19:58:35 +0200

    Forum view styled

--- client/src/components/PostItem.jsx
+++ client/src/components/PostItem.jsx
@@ -1,15 +1,41 @@
+import { Row, Col, Image, Button } from "react-bootstrap";
+import "./PostItem.scss";
+
 const PostItem = ({ post }) => {
+  const user = post.creator;
   return (
     <>
-      <div className="card__post-list">
-        <div className="post-item__details">
-          <img src="" alt=""/>
-          <h3>{post.title}</h3>
-          <small>
-            {post.creator.username}
-          </small>
-        </div>
-      </div>
+      <Row>
+        <Col className="postItem mb-0" md={12}>
+          <div>
+          <div className="post_item__user">
+            {user.profilePicture ? (
+              <Image roundedCircle  src={user.profilePicture} alt={user.username} />
+            ) : (
+              <Image
+                roundedCircle 
+                src="https://source.unsplash.com/300x300/?smiling,woman"
+                alt={user.username}
+              />
+            )}
+            <strong>{user.username}</strong>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <h3>{post.title}</h3>
+            <small>
+              {new Date(post.addedDate).toLocaleDateString("en-GB")} <br />
+            </small>
+          </div>
+          <div className="postButton">
+          <Button>
+            View
+          </Button>
+
+          </div>
+          </div>
+        </Col>
+      </Row>
     </>
   );
 };
